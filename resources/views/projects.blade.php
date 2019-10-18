42396559c27aabbd63db76e36191e7a697abc202
Muhammad Orabi <orabi.dev@gmail.com>
2019-10-18 18:48:48 +0200

    update projects

--- resources/views/projects.blade.php
+++ resources/views/projects.blade.php
@@ -12,25 +12,24 @@
 <div class="mt-12 spaced-y-10">
     <div>
         <p class="text-lg text-black font-bold no-underline">
-            Voting App
+            Digital Assistant
         </p>
         <p class="text-grey-darkest text-base leading-normal mt-1">
-            it’s a voting app, so you can register and create polls and people are allowed to vote and there is a
-            data visualization for the votes.
+            simple Appointment web application between doctors and patients using nodejs, python and
+            golang for the back-end. Patient can view all the available time slots of a doctor so he would be able to make
+            an appointment.
             <ul>
                 <li>
-                    PHP, Laravel 5.4, Vue.Js 2.0 , Bootstrap V4
+                    REST
                     <div class="text-grey-darkest text-base leading-normal mt-2">
-                        [<a href="https://github.com/MuhammadOrabi/voting-app" target="__blank" class="text-grey-darker hover:text-black text-sm no-underline hover:underline">Github Repo</a> |
-                        <a href="http://vote.orabi.me/" target="__blank" class="text-grey-darker hover:text-black text-sm no-underline hover:underline">Live</a>]
+                        [<a href="https://github.com/MuhammadOrabi/assessment-task" target="__blank" class="text-grey-darker hover:text-black text-sm no-underline hover:underline">Github Repo</a>]
                     </div>
                 </li>
                 <br>
                 <li>
-                    Bootstrap V4, Express.js, MongoDB, React.js
+                    Messaging over Message Broker using RabbitMQ (under development)
                     <div class="text-grey-darkest text-base leading-normal mt-2">
-                        [<a href="https://github.com/MuhammadOrabi/voting-app-react" target="__blank" class="text-grey-darker hover:text-black text-sm no-underline hover:underline">Github Repo</a> |
-                        <a href="http://vote.react.orabi.me/" target="__blank" class="text-grey-darker hover:text-black text-sm no-underline hover:underline">Live</a>]
+                        [<a href="https://gitlab.com/digital-assistant/app" target="__blank" class="text-grey-darker hover:text-black text-sm no-underline hover:underline">Gitlab Repo</a>]
                     </div>
                 </li>
             </ul>
@@ -59,24 +58,25 @@
 
     <div>
         <p class="text-lg text-black font-bold no-underline">
-            Digital Assistant
+            Voting App
         </p>
         <p class="text-grey-darkest text-base leading-normal mt-1">
-            simple Appointment web application between doctors and patients using nodejs, python and
-            golang for the back-end. Patient can view all the available time slots of a doctor so he would be able to make
-            an appointment.
+            it’s a voting app, so you can register and create polls and people are allowed to vote and there is a
+            data visualization for the votes.
             <ul>
                 <li>
-                    REST
+                    PHP, Laravel 5.4, Vue.Js 2.0 , Bootstrap V4
                     <div class="text-grey-darkest text-base leading-normal mt-2">
-                        [<a href="https://github.com/MuhammadOrabi/assessment-task" target="__blank" class="text-grey-darker hover:text-black text-sm no-underline hover:underline">Github Repo</a>]
+                        [<a href="https://github.com/MuhammadOrabi/voting-app" target="__blank" class="text-grey-darker hover:text-black text-sm no-underline hover:underline">Github Repo</a> |
+                        <a href="http://vote.orabi.me/" target="__blank" class="text-grey-darker hover:text-black text-sm no-underline hover:underline">Live</a>]
                     </div>
                 </li>
                 <br>
                 <li>
-                    Messaging over Message Broker using RabbitMQ (under development)
+                    Bootstrap V4, Express.js, MongoDB, React.js
                     <div class="text-grey-darkest text-base leading-normal mt-2">
-                        [<a href="https://gitlab.com/digital-assistant/app" target="__blank" class="text-grey-darker hover:text-black text-sm no-underline hover:underline">Gitlab Repo</a>]
+                        [<a href="https://github.com/MuhammadOrabi/voting-app-react" target="__blank" class="text-grey-darker hover:text-black text-sm no-underline hover:underline">Github Repo</a> |
+                        <a href="http://vote.react.orabi.me/" target="__blank" class="text-grey-darker hover:text-black text-sm no-underline hover:underline">Live</a>]
                     </div>
                 </li>
             </ul>
