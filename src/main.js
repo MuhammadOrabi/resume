f89c14a5e0fae985c4934cd740cc5f77317f4c5b
Muhammad Orabi <m.orabi@travelyalla.com>
2023-02-05 15:14:05 +0200

    add google analytics

--- src/main.js
+++ src/main.js
@@ -2,5 +2,8 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
+import VueGtag from "vue-gtag";
 
-createApp(App).use(router).mount('#app')
+createApp(App).use(router).use(VueGtag, {
+    config: {id: 'G-FW8QJFV82L'}
+}, router).mount('#app')
